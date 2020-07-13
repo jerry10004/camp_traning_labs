94ab63b29ae8afeb335377222e111558d9081f3e
jsy <jerry1004@handong.edu>
2020-07-13 22:32:13 +0900

    update 3_WebApp

--- 3_SimpleWebApp/app/src/main/java/com/example/a3_simplewebapp/MainActivity.java
+++ 3_SimpleWebApp/app/src/main/java/com/example/a3_simplewebapp/MainActivity.java
@@ -3,12 +3,43 @@ package com.example.a3_simplewebapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.webkit.WebResourceRequest;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+
+import com.google.android.material.snackbar.Snackbar;
 
 public class MainActivity extends AppCompatActivity {
+    WebView webView;
+    WebSettings webSettings;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        webView = findViewById(R.id.webView);
+
+        webView.setWebViewClient(new WebViewClient(){
+            @Override
+            public boolean shouldOverrideUrlLoading(WebView view, WebResourceRequest request) {
+                Snackbar.make(view, view.getUrl().toString(), Snackbar.LENGTH_LONG).setAction("OK", new View.OnClickListener() {
+                    @Override
+                    public void onClick(View view) {
+
+                    }
+                }).show();
+                return super.shouldOverrideUrlLoading(view, request);
+            }
+        });
+
+        webSettings = webView.getSettings();
+        webSettings.setDisplayZoomControls(true);
+        webSettings.setSupportZoom(true);
+        webSettings.setJavaScriptEnabled(true);
+
+        webView.loadUrl("https://github.com");
     }
 }
